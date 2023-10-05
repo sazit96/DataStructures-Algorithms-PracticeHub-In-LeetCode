e775b83e059f8c747baf5feb08083c076cc3d953
Ehosanul Islam Sazit <sazitislam96@gmail.com>
2023-10-05 19:00:00 +0600

    update week 1 Day 5

---
+++ 1.Week(Arrays)/Day 5 (Stack)/20. Valid Parentheses.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>st;
+        for(char it : s)
+        {
+            if(it=='(' || it=='{' || it=='[') st.push(it);
+            else
+            {
+                if(st.empty()) return false;
+                if(it == ')' && st.top() != '(') return false;
+                if(it=='}' && st.top() != '{') return false;
+                if(it==']' && st.top() != '[') return false;
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
